1115c6264c4db41c7d4b1a477b8a46c09563ce68
wpgodforever <997587729@qq.com>
2023-02-27 14:46:25 +0800

    [add]登录功能实现(密码未加密)

--- router/user.js
+++ router/user.js
@@ -42,24 +42,46 @@ router.post('/register',(req, res) =>{
 
 
 router.get('/login',(req,res) =>{
-    console.log('访问登录接口')
-    // jwt生成token 
-    // let token = jwt.sign({请求里的信息},加密密码，{expiresIn过期时间，algorithm加密方式})
-    // res.json({
-    //     code:200,
-    //     msg:'注册成功',
-    //     token
-    // })
-    // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IndwIiwiaWF0IjoxNjc3NDE1OTIyLCJleHAiOjE2Nzc0MTY1MjJ9.Bg0I_6L5BOXp_l9mHTzw5ZuSR7K72CjCTt-5tUCZOiQ
-    let token = jwt.sign({username:"wp"},'wp124',{
-        expiresIn:'600s',
-        algorithm: 'HS256'
+    const username = req.query.username
+    const password = req.query.password
+    if(!username){
+        responseClient(res,400,1,'请输入用户名')
+        return
+    }
+    if(!password){
+        responseClient(res,400,1,'请输入密码')
+        return
+    }
+    User.findOne({
+        username
+    }).then(data => {
+        if(!data){
+            responseClient(res,400,1,'该用户不存在')
+        }else{
+            if(data.password !== password){
+                responseClient(res,400,1,'密码错误')
+                return
+            }
+            // jwt生成token 
+            // let token = jwt.sign({请求里的信息},加密密码，{expiresIn过期时间，algorithm加密方式})
+            // res.json({
+            //     code:200,
+            //     msg:'注册成功',
+            //     token
+            // })
+            // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IndwIiwiaWF0IjoxNjc3NDE1OTIyLCJleHAiOjE2Nzc0MTY1MjJ9.Bg0I_6L5BOXp_l9mHTzw5ZuSR7K72CjCTt-5tUCZOiQ
+            let token = jwt.sign({username, password},'wp124',{
+                expiresIn:'6000s',
+                algorithm: 'HS256'
+            })
+            res.json({
+                code:200,
+                msg:'登录成功',
+                token
+            })
+        }
     })
-    res.json({
-        code:200,
-        msg:'注册成功',
-        token
-    })
+    
 })
 
 router.post('/test',(req,res) => {
