3e4f3651e9c1baefcaa2289d0344b3acc3d56e3f
wpgodforever <997587729@qq.com>
2023-03-14 18:25:22 +0800

    【add】返回列表新增图片

--- router/article.js
+++ router/article.js
@@ -176,6 +176,7 @@ router.get('/list',async(req, res) => {
             tags:1,
             updatedAt:1,
             createdAt:1,
+            cover:1,
         }).then((articleRes, articleReq) => {
         if(!articleRes) return responseClient(res,200,400,'没找到该文章')
         responseClient(res,200,200,'查询成功',
