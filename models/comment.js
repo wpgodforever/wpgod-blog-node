ec3bd28d4351092f9922f870def5bff5fbf74fa7
wpgodforever <997587729@qq.com>
2023-03-17 17:39:56 +0800

    【add】评论表管联回复表查询

--- models/comment.js
+++ models/comment.js
@@ -29,7 +29,18 @@ const CommentSchema = new Schema(
         timestamps:true
     }
 )
-
+// 评论关联一对多(查出该条评论的所有回复)
+// 用虚拟字段virtual
+CommentSchema.virtual("coms",{
+    ref: "Reply",
+    localField: "_id",//用当前评论的id 关联回复表里article_id
+    foreignField: "reply_common_id",
+    justOne: false,//全部回复
+    // count: true //若为true则只显示数组长度，不显示数组内容
+})
+// 下面这两句必须要写，这样虚拟字段才可以显性看到
+CommentSchema.set("toObject", {virtuals:true})
+CommentSchema.set("toJSON", {virtuals:true})
 const Comment = mongodb.mongoose.model('Comment',CommentSchema)
 
 // 回复评论表
@@ -42,7 +53,7 @@ const ReplySchema = new Schema(
         reply_common_id: {//回复的那条评论的id
             type:Schema.Types.ObjectId,
             required: true,
-            ref: 'Article'
+            ref: 'Comment'
         },
         // 回复的人的id
         reply_user_id:{
@@ -55,6 +66,11 @@ const ReplySchema = new Schema(
             type:Schema.Types.ObjectId,
             required: true,
             ref: 'User'
+        },
+        article_id: {//该回复在那个文章里
+            type:Schema.Types.ObjectId,
+            required: true,
+            ref: 'Article'
         },
     },
     {
